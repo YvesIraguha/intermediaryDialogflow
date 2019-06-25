d0205f39921955d4c43da5488c62bda5211957bf
YvesIraguha <iraguhaivos@gmail.com>
2019-06-25 22:16:20 +0200

    fix the result from api

--- index.js
+++ index.js
@@ -21,9 +21,61 @@ app.post('/', (req, res) => {
   console.log('request', req.body);
   //   response.setHeader('Content-Type', 'application/json');
   return res.json({
-    speech: 'Something went wrong!',
-    displayText: 'Something went wrong!',
-    source: 'get-movie-details'
+    fulfillmentText: 'This is a text response',
+    fulfillmentMessages: [
+      {
+        card: {
+          title: 'card title',
+          subtitle: 'card text',
+          imageUri:
+            'https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png',
+          buttons: [
+            {
+              text: 'button text',
+              postback: 'https://assistant.google.com/'
+            }
+          ]
+        }
+      }
+    ],
+    source: 'example.com',
+    payload: {
+      google: {
+        expectUserResponse: true,
+        richResponse: {
+          items: [
+            {
+              simpleResponse: {
+                textToSpeech: 'this is a simple response'
+              }
+            }
+          ]
+        }
+      },
+      facebook: {
+        text: 'Hello, Facebook!'
+      },
+      slack: {
+        text: 'This is a text response for Slack.'
+      }
+    },
+    outputContexts: [
+      {
+        name:
+          'projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name',
+        lifespanCount: 5,
+        parameters: {
+          param: 'param value'
+        }
+      }
+    ],
+    followupEventInput: {
+      name: 'event name',
+      languageCode: 'en-US',
+      parameters: {
+        param: 'param value'
+      }
+    }
   });
 });
 // app.get('/', (req, res) => {
